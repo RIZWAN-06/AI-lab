6f147b23f3fb701dff1442afde91993df7160660
RIZWAN-06 <rizu33451@gmail.com>
2026-08-28 22:58:33 +0530

    OOP-Attributes-and-Class-keyword

--- OOP-Attributes-and-Class-keyword.py
+++ OOP-Attributes-and-Class-keyword.py
@@ -28,6 +28,11 @@ print(my_dog.MyAttribute)
 
 #now let's create more attributes
 class Dog():
+
+    #Class object attributes
+    #Same for any instance of a Class
+    species = 'Mammal'
+
     def __init__(self,breed,name,spots):
         #attributes
         #we take in the arguments
@@ -35,11 +40,53 @@ class Dog():
         self.breed = breed
         self.name = name
         self.spots = spots  #here we are expecting boolean True/False
+    
+    #Operations/Action ---> Methods
+    
+    def bark(self,number): #Methos can take other arguments too 
+        print('WOOF! My name is {} and the number is {}'.format(self.name,number))  
+        #So notice here we are no longer saying {self} because number is already gonna be provided by the user 
 
+
+
+#now let's create the instance
 my_dog = Dog(breed = 'Huskie', name = 'Sam', spots = False)
 
-print(my_dog.breed, my_dog.name, my_dog.spots)
+print(my_dog.breed, my_dog.name, my_dog.spots, my_dog.species)
+print(my_dog.species)
+print(my_dog.bark(20))
 
 
 
 
+#NOW LETS CREATE A NEW CLASS FOR CLEARENCE EVERYTHING
+
+#Let's create a class called Circle
+class Circle():
+    #class object attribute 
+    pi = 3.14  #so regardless of instance of a Circle i make i'll always beable to referance Pi here as 3.14
+
+    def __init__(self,radious = 1): #seting radious default value 
+        self.radious = radious
+        #attribute doesn't necessary have to be define from a particular call
+        self.area = radious*radious*self.pi   #Since Pi is a class object attribute we can simply call it by the name of the class
+
+
+        #now lets make a method here 
+    def get_circumference(self):
+        return self.radious*Circle.pi*2
+
+
+#Let's create the instance
+my_circle = Circle(30)  #We can provide value for radious
+
+#Let's call it 
+
+print(my_circle.pi)
+print(my_circle.radious) 
+
+#now let's see how the Method is working
+print(my_circle.get_circumference())
+
+#Let's call the area 
+print(my_circle.area)
