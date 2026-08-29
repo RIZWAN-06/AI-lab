d2e3ab4611606033709903ae281b5364a2749436
RIZWAN-06 <rizu33451@gmail.com>
2026-08-29 21:16:10 +0530

    OOP-Inheritance-And-Polymorphism

---
+++ OOP-Inheritance-And-Polymorphism.py
@@ -0,0 +1,6 @@
+#Inheritance --> It's a way to form new classes using classesthat have already been defined.
+
+#Let's create a simple base class, to understand it better
+class Animal():
+    def __init__(self):
+        print ("Animal created")
