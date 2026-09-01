650667c763c12765766bf60d24524ccbdec44c88
RIZWAN-06 <rizu33451@gmail.com>
2026-09-01 22:34:48 +0530

    OOP-SpaSpecial_Magic-Dunder_Methods

---
+++ OOP-Special_Magic-Dunder_Methods.py
@@ -0,0 +1,12 @@
+#Special Methods --> it's allow us to use some built-in operations in python, such as the Length function or the print function with our own user-created objects
+
+#Lets see a simple example 
+mylist = [1,2,3]
+
+print(mylist)
+
+len(mylist)
+
+
+
+
