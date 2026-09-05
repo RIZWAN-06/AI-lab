35e2698fbda926c6b885107663bb951e29ee38c8
RIZWAN-06 <rizu33451@gmail.com>
2026-09-05 23:26:52 +0530

    Modules-And-Packages

--- Modules-And-Packages/myprogram.py
+++ Modules-And-Packages/myprogram.py
@@ -1,3 +1,9 @@
+from MyMainPackage import some_main_script
+from MyMainPackage.subpackage import mysubscript
+
 from mymodule import my_func
 
 my_func()
+
+some_main_script.report_main()
+mysubscript.sub_report()
